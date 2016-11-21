67169700e96bde8f03cfa2fe9f9f410677043bd7
demist <mail@starichkov.com>
2016-11-22 00:25:14 +0300

    git fix

--- transactions_server.erl
+++ transactions_server.erl
@@ -3,35 +3,54 @@
 
 -export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+-spec get_timestamp() -> integer().
+get_timestamp() ->
+  {Mega, Sec, Micro} = os:timestamp(),
+  (Mega*1000000 + Sec)*1000 + round(Micro/1000).
+
 start_link() ->
     gen_server:start_link({global, transactions_server}, transactions_server, [], []).
+
+parse([]) -> [];
+
+parse([[{Key, First}] | Rest]) ->
+    Left = parse(Rest),
+    Res = lists:append([{Key, First}], Left),
+    Res.
     
 init([]) ->
+    Key = get_timestamp(),
     process_flag(trap_exit, true),
-    {ok, Ref} = dets:open_file(mydata, []),
-    State = dets:match(Ref, '$1'),
-    {ok, {State, Ref}}.  
+    {ok, Ref} = dets:open_file(d4, []),
+    F = dets:match(Ref, '$1'),
+    Res1 = parse(F),
+    Res2 = lists:sort(fun({K1, _V1}, {K2, _V2}) -> K1 < K2 end, Res1),
+    Res3 = lists:map(fun ({_, {[V]}}) -> V end, Res2),
+    {ok, {Key, Res3, Ref}}.  
     
-handle_call(history, _From, {State, Ref}) ->
-    {reply, State, {State, Ref}};
+handle_call(history, _From, {Key, State, Ref}) ->
+    {reply, State, {Key, State, Ref}};
    
-handle_call(_Message, _From, {State, Ref}) ->
-    {reply, invalid_command, {State, Ref}}.
+handle_call(_Message, _From, {Key, State, Ref}) ->
+    {reply, invalid_command, {Key, State, Ref}}.
     
-handle_cast({widthdraw, Amount}, {State, Ref}) ->
+handle_cast({widthdraw, Amount}, {Key, State, Ref}) ->
     NewState = lists:append(State, Amount),
-    dets:insert(Ref, {Amount}),
-    {noreply, {NewState, Ref}};
+    NewKey = Key + 1,
+    dets:insert(Ref, {NewKey, {Amount}}),
+    {noreply, {NewKey, NewState, Ref}};
     
-handle_cast(clear, {_State, Ref}) ->
+handle_cast(clear, {_Key, _State, Ref}) ->
     NewState = [],
-    {noreply, {NewState, Ref}};
+    ok = dets:delete_all_objects(Ref),
+    NewKey = get_timestamp(),
+    {noreply, {NewKey, NewState, Ref}};
 
 handle_cast(_Message, State) -> {noreply, State}.      
 
 handle_info(_Message, State) -> {noreply, State}.
 
-terminate(_Reason, {_State, Ref}) -> 
+terminate(_Reason, {_Key, _State, Ref}) -> 
     dets:close(Ref),
     ok.
 
